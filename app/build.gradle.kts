86444acf4139631bb0e9a2a712fa85190157e0c7
kadirkid <abdulahiosoble96@gmail.com>
2023-09-21 15:42:11 -0400

    Remove palette

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,6 @@ kotlin {
                 implementation(projects.serviceApi)
                 implementation(libs.koin.core)
                 implementation(libs.kotlinx.coroutines.core)
-                implementation(libs.palette.multiplatform)
             }
         }
 
